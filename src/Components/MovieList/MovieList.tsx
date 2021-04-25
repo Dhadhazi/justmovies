6d16e6d730b8b0577699e0e26ee6683567e1a8fa
Dhadhazi <dhadhazi@gmail.com>
2021-04-25 15:00:17 +0200

    Getting moveis with axios

--- src/Components/MovieList/MovieList.tsx
+++ src/Components/MovieList/MovieList.tsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { MovieBox } from "./Components/MovieBox";
+import axios from "axios";
+
+interface Props {
+  search?: string;
+}
+
+interface Movies {
+  image: string;
+  title?: string;
+  date?: string;
+  imdbID?: string;
+}
+
+interface OMDBSearchResultData {
+  Poster: string;
+  Title: string;
+  Year: string;
+  imdbID: string;
+}
 
 const MainContainer = styled.div`
   display: flex;
@@ -32,18 +51,44 @@ const MovieListContainer = styled.div`
   }
 `;
 
-export const MovieList = () => {
+export const MovieList = ({ search }: Props) => {
+  const [movies, setMovies] = useState<Movies[]>();
+
+  useEffect(() => {
+    const data = axios
+      .get("http://www.omdbapi.com/?s=%22star%20wars%22&apikey=3b66bce")
+      .then((res) => {
+        const data = res.data.Search;
+        console.log(data);
+        const cleaned = data.map((movie: OMDBSearchResultData) => {
+          return {
+            title: movie.Title,
+            date: movie.Year,
+            image: movie.Poster,
+            imdbId: movie.imdbID,
+          };
+        });
+        setMovies(cleaned);
+      });
+
+    setMovies([
+      {
+        title: "Guardians of the galax",
+        image:
+          "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
+      },
+    ]);
+  }, [search]);
+
   return (
     <MainContainer>
       <ContentContainer>
-        <Title>You might like</Title>
+        <Title>{search ? "Search Results" : "You might like"}</Title>
         <MovieListContainer>
-          <MovieBox image="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" />
-          <MovieBox
-            title="Guardians of the Galaxy"
-            date="2014"
-            image="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"
-          />
+          {movies &&
+            movies.map(({ title, image, date, imdbID }: Movies) => (
+              <MovieBox title={title} date={date} image={image} key={imdbID} />
+            ))}
         </MovieListContainer>
       </ContentContainer>
     </MainContainer>
